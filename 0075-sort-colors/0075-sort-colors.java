d14b5d6f50bdaf8cdcfdb24014e9fcd04b65c50b
yagamio <116731864+yagamio@users.noreply.github.com>
2023-05-05 07:02:44 +0200

    Time: 1 ms (19.77%), Space: 41.1 MB (61.50%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,58 +1,61 @@
 /**
  * 75. Sort Colors
- * Shell.
- * n is the length of nums.
- * Time complexity: O(n^(4/3)).
+ * Three way quick sort.
+ * n is the size of nums.
+ * Time complexity: O(n) - O(nlogn).
  * Space complexity: O(1).
  */
 class Solution {
-    
     public void sortColors(int[] nums) {
         Integer[] boxedNums = new Integer[nums.length];
         for (int i = 0; i < nums.length; i++) {
             boxedNums[i] = nums[i];
         }
-        Shell sorter = new Shell<Integer>();
+        threeWayQuickSort sorter = new threeWayQuickSort<>();
         sorter.sort(boxedNums);
         for (int i = 0; i < nums.length; i++) {
             nums[i] = boxedNums[i];
         }
     }
 
-    public class Shell<T extends Comparable<T>> extends sort<T> {
-
+    public class threeWayQuickSort<T extends Comparable<T>> extends sort<T> {
         @Override
         public void sort(T[] nums) {
-            int N = nums.length;
-            int h = 1;
-            while (h < N / 3) {
-                h = 3 * h + 1;
-            }
-            while (h >= 1) {
-                for (int i = h; i < N; i++) {
-                    for (int j = i; j >= h && less(nums[j], nums[j - h]); j -= h) {
-                        swap(nums, j, j - h);
-                    }
-                }
-                h = h / 3;
-            }
+            sort(nums, 0, nums.length - 1);
         }
-
+        
+        protected void sort(T[] nums, int l, int h) {
+            if (h <= l) return;
+            // 定义三个指针
+            int lt = l, i = l + 1, gt = h;
+            // 取第一个数作为基准
+            T v = nums[l];
+            while (i <= gt) {
+                int compare = nums[i].compareTo(v);
+                if (compare == 0) {
+                    i++;
+                } else if (compare > 0) {
+                    swap(nums, i, gt--);
+                } else {
+                    swap(nums, i++, lt++);
+                }
+            }
+            sort(nums, l, lt - 1);
+            sort(nums, gt + 1, h);
+        }
     }
 
     public abstract class sort<T extends Comparable<T>> {
-        
-        public void sort(T[] nums) {};
+        public void sort(T[] nums){};
 
-        public boolean less(T v, T w) {
+        protected boolean less(T v, T w) {
             return v.compareTo(w) < 0;
         }
 
-        public void swap(T[] a, int i, int j) {
+        protected void swap(T[] a, int i, int j) {
             T t = a[i];
             a[i] = a[j];
             a[j] = t;
         }
-
     }
 }
