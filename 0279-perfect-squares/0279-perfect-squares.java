13dadf8bf2be0678124394a73bfdf7a50a0d6946
HE Zhicheng <116731864+yagamio@users.noreply.github.com>
2023-07-17 09:41:16 +0200

    Time: 183 ms (16.19%), Space: 43.5 MB (21.51%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,36 +1,25 @@
 /**
  * 279. Perfect Squares
- * BFS.
- * Time complexity: O(n * sqrt(n)). 因为求平方数的时间复杂度是sqrt(n).
- * Space complexity: O(n).
+ * Dynamic programming
  */
 class Solution {
     public int numSquares(int n) {
-        List<Integer> squares = getSquares(n);
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-        queue.add(n);
-        visited[n] = true;
-        int level = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            level++;
-            while (size-- > 0) {
-                int cur = queue.poll();
-                for (int s : squares) {
-                    int next = cur - s;
-                    if (next < 0) break;
-                    if (next == 0) return level;
-                    if (visited[next]) continue;
-                    visited[next] = true;
-                    queue.add(next);
+        List<Integer> squares = generateSquares(n);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE; // record the current minimum
+            for (int square : squares) {
+                if (square > i) {
+                    break;
                 }
+                min = Math.min(min, dp[i - square] + 1);
             }
+            dp[i] = min;
         }
-        return n;
+        return dp[n];
     }
 
-    protected List<Integer> getSquares(int n) {
+    private List<Integer> generateSquares(int n) {
         List<Integer> squares = new ArrayList<>();
         int square = 1;
         int diff = 3;
