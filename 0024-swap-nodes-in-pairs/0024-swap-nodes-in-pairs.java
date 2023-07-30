57d242c7eeb7c886a1575615a2383d23ebc36251
HE Zhicheng <116731864+yagamio@users.noreply.github.com>
2023-07-30 08:40:12 +0200

    Time: 0 ms (100.00%), Space: 40.2 MB (70.06%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -9,20 +9,24 @@
  * }
  */
 
- /**
-  * 24. Swap Nodes in Pairs
-  * Recursion.
-  * Time complexity: O(N).
-  * Space complexity: O(N).
-  */
+/**
+ * 24. Swap Nodes in Pairs
+ * Iteration
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) { // exit condition
-            return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null && head.next != null) {
+            pre.next = head.next;
+            head.next = head.next.next;
+            pre.next.next = head;
+            pre = head;
+            head = head.next;
         }
-        ListNode next = head.next; // let the second element be head
-        head.next = swapPairs(head.next.next); // head points to later nodes (swapped)
-        next.next = head; // second node points to the first (swap them)
-        return next;
+        return dummy.next;
     }
 }
