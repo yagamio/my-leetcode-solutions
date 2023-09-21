148274bb5e916832bcd3d4de0d03d921b03e472e
Zhi-Cheng HE <116731864+yagamio@users.noreply.github.com>
2023-09-21 11:13:11 +0200

    Time: 1 ms (98.70%), Space: 40.1 MB (97.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,27 @@
 /**
  * 20. Valid Parentheses
- * Stack.
- * N is the length of string.
- * Time complexity: O(N).
- * Space complexity: O(N).
+ * Stack
+ * N is the length of string s
+ * Time complexity: O(N)
+ * Space complexity: O(N)
  */
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() == 0 || s.length() == 1) return false;
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '('|| c == '[' || c == '{') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (stack.isEmpty() || stack.pop() != '(') return false;
-            } else if (c == ']') {
-                if (stack.isEmpty() || stack.pop() != '[') return false;
-            } else if (c == '}') {
-                if (stack.isEmpty() || stack.pop() != '{') return false;
+        for (char a : s.toCharArray()) {
+            if (a == '(' || a == '[' || a == '{') {
+                stack.push(a);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (a == ')') {
+                if (stack.pop() != '(') return false;
+            } else if (a == ']') {
+                if (stack.pop() != '[') return false;
+            } else if (a == '}') {
+                if (stack.pop() != '{') return false;
             }
         }
-        return stack.isEmpty();
+        if (!stack.isEmpty()) return false;
+        return true;
     }
 }
