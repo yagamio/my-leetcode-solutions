95d2d860a20edab810e93fef6d6fa4a7c7edff51
Zhi-Cheng HE <116731864+yagamio@users.noreply.github.com>
2023-09-26 08:28:09 +0200

    Time: 5 ms (60.77%), Space: 43.4 MB (91.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,19 @@
+/**
+ * 1. Two Sum
+ * HashMap
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
 class Solution {
-    // HashMap.
-    // N is the size of nums.
-    // Time complexity: O(N).
-    // Space complexity: O(N).
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> differences = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        // check if (target - num) exists in the Hash map
-        for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i];
-            if (map.containsKey(diff) && map.get(diff) != i) {
-                result[0] = i;
-                result[1] = map.get(diff);
-                return result;
+            if (!differences.containsKey(nums[i])) {
+                differences.put(target - nums[i], i);
+            } else {
+                result[0] = differences.get(nums[i]);
+                result[1] = i;
             }
         }
         return result;
