3c24c86b752cdb07ad29e9a8a7b34b3290e48442
Zhi-Cheng HE <116731864+yagamio@users.noreply.github.com>
2023-10-23 15:35:00 +0200

    Time: 4 ms (84.39%), Space: 56.2 MB (69.77%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,22 @@
+/**
+ * 287. Find the Duplicate Number
+ * Double pointers
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
