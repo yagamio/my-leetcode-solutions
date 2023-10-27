4361d7329e4c27e72af442afb076667667809ccf
Zhi-Cheng HE <116731864+yagamio@users.noreply.github.com>
2023-10-27 12:13:37 +0200

    Time: 14 ms (83.62%), Space: 46.7 MB (32.64%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -0,0 +1,32 @@
+/**
+ * 697. Degree of an Array
+ * HashMap
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        int result = nums.length;
+        int maxFre = 0;
+        Map<Integer, int[]> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            if (!map.containsKey(cur)) {
+                map.put(cur, new int[] {1, i, i});
+            } else {
+                int[] array = map.get(nums[i]);
+                array[0] = array[0] + 1;
+                array[2] = i;
+            }
+            maxFre = Math.max(maxFre, map.get(nums[i])[0]);
+        }
+        for (int x : map.keySet()) {
+            int[] array = map.get(x);
+            if (array[0] == maxFre) {
+                int len = array[2] - array[1] + 1;
+                result = Math.min(result, len);
+            }
+        }
+        return result;
+    }
+}
